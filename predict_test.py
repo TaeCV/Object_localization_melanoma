4e117626d28b42a553eac43a7159fcb92f01cf93
Chayakorn Vongbunsin <taecv.work@gmail.com>
2022-01-14 23:34:52 +0700

    change variable name

--- predict_test.py
+++ predict_test.py
@@ -1,8 +1,8 @@
 from mmdet.apis import inference_detector, init_detector, show_result_pyplot
 
 MODEL_CONFIG = './model_config_SmoothL1.py'
-MODEL_PTH = "./mask_rcnn_smoothl1.pth"
+CHECKPOINT_PTH = "./mask_rcnn_smoothl1.pth"
 IMG_PTH = "./example.jpg"
-Mask_rcnn_model = init_detector(MODEL_CONFIG, MODEL_PTH, device='cuda:0')
-result = inference_detector(Mask_rcnn_model, IMG_PTH)
-show_result_pyplot(Mask_rcnn_model, IMG_PTH, result, score_thr=0.3)
+mask_rcnn_model = init_detector(MODEL_CONFIG, CHECKPOINT_PTH, device='cuda:0')
+result = inference_detector(mask_rcnn_model, IMG_PTH)
+show_result_pyplot(mask_rcnn_model, IMG_PTH, result, score_thr=0.3)
